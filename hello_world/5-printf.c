84e3eac1dcdbb1dfeada1e2fca64053bf48fcda7
marioabou <7655@holbertonstudents.com>
2023-10-18 14:29:18 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - A program that prints a line uning the printf function
  * Retorn: 0 (succes)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	return (0);
 }
