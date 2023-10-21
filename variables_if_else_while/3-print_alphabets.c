47e2c9d2eb648205a6d6954581332bff77aa1b45
marioabou <7655@holbertonstudents.com>
2023-10-21 14:06:41 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+char d;
+
+c = 'a';
+d = 'z';
+while
+(c <= 'z') {
+putchar(c);
+c++;  }
+while
+(d <= 'z') {
+putchar(d);
+d++; }
+putchar('\n');
+return (0);
+}
