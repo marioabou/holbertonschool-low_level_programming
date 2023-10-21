22112a404ffa0841150ab318823afbe46d7b44ed
marioabou <7655@holbertonstudents.com>
2023-10-21 13:17:50 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry piont
+ * Return: 0
+ */
+
+int main(void)
+{
+char c;
+
+c = 'a';
+while 
+(c <= 'z') {
+putcher(c);
+c++; }
+putchar('\n');
+return (0);
+}
