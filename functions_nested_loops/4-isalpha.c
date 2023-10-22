6b37ac787ab704ea6a0b64938f2af99a8ebfc2ec
marioabou <7655@holbertonstudents.com>
2023-10-22 16:05:33 -0700

    4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - Check for lowercase character
+ * _isalpha - Check for alphabetic character
  * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
