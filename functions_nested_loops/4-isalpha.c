74649a1ec27009368b809f0ff0736eeaa324bf83
marioabou <7655@holbertonstudents.com>
2023-10-22 15:57:12 -0700

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Check for lowercase character
+ * @c: The character to be checked
+ * Return: 1 for lowercase character or 0 for anything else
+ */
+
+int :isalpha(int c)
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
+}
