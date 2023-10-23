d4156981c261dbe75282277fdaf626e2fa29ea8e
marioabou <7655@holbertonstudents.com>
2023-10-22 17:05:52 -0700

    5-sign.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abc - Computes de absolute value of an integer
+ * @c: The character to be computed
+ * Return: An absolute value of number or zero
+ */
+
+int _abs(int c)
+{
+if (c < 0)
+{
+int ads_val;
+
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
