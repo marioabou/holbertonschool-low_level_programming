ba7dd5a8b92f0d72b7088c09685a7846ff720e4b
marioabou <7655@holbertonstudents.com>
2023-10-22 18:46:38 -0700

    10-add.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,7 +7,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _add(int);
+int add(int);
 void print_to_98(int n);
 void print_times_table(int n);
 int _abs(int);
