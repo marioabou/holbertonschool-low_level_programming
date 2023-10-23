8a8e89cb742dca4c366045da5f6bfee843b9aee9
marioabou <7655@holbertonstudents.com>
2023-10-22 19:01:16 -0700

    10-add.c

--- functions_nested_loops/10-main.c
+++ functions_nested_loops/10-main.c
@@ -1,13 +1,3 @@
-Enumerating objects: 7, done.
-Counting objects: 100% (7/7), done.
-Delta compression using up to 2 threads
-Compressing objects: 100% (4/4), done.
-Writing objects: 100% (4/4), 402 bytes | 402.00 KiB/s, done.
-Total 4 (delta 3), reused 0 (delta 0)
-remote: Resolving deltas: 100% (3/3), completed with 3 local objects.
-To https://github.com/marioabou/holbertonschool-low_level_programming.git
-   c272627..68f32f3  master -> master
-root@65051705e1fd:/holbertonschool-low_level_programming/functions_nested_loops# vi 10-main.c
 #include "main.h"
 #include <stdio.h>
 
@@ -18,9 +8,9 @@ root@65051705e1fd:/holbertonschool-low_level_programming/functions_nested_loops#
  */
 int main(void)
 {
-int n;
+    int n;
 
-n = add(89, 9);
-printf("%d\n", n);
-return (0);
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
 }
