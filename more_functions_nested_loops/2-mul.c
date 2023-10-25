99104136a26a4cf6d9d6ae47e7db46b492ebb7f5
marioabou <7655@holbertonstudents.com>
2023-10-25 16:12:08 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function check for a digit (0 trough)
+ * @a: a - variable
+ * @b: b - variable
+ * Return: Always 0
+ */
+int mul (int a, int b)
+{
+int mul;
+
+mul = a * b;
+return (mul);
+}
