080813fd15317a217fcb0a31b0cae9e225d254bf
marioabou <7655@holbertonstudents.com>
2023-10-25 16:20:51 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - function check for a digit (0 trough 9)
+ * 
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+int c;
+for (c = 48; c < 58; c++);
+{
+_putchar(c);
+}
+_putchar('\n');
+}
