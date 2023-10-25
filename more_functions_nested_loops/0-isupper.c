997e0233e9d2f6c35e514659cbe89ac3a76ccbdc
marioabou <7655@holbertonstudents.com>
2023-10-25 15:52:54 -0700

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - prog check for uppercase character
+ * @c: variable text
+ * Return: always 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c<= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
