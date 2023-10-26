61ab28adde524b8ba53cb9e6f02425a83831bd22
marioabou <7655@holbertonstudents.com>
2023-10-25 17:05:00 -0700

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,7 +4,7 @@
  * more_numbers - function that prints 10 times the number, from 0 to 14
  * followed by a new line
  */
-void more_numbers(void) 
+void more_numbers(void)
 {
 int i, j;
 for (i = 0; i < 10; i++)
