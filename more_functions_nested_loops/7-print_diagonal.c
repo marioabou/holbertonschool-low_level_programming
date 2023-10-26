b9c1d594e73d7e7d8916b0d8c0c4827b243fda71
marioabou <7655@holbertonstudents.com>
2023-10-25 17:29:48 -0700

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,7 +6,7 @@
  */
 void print_diagonal(int n)
 {
-if (n <=0)
+if (n <= 0)
 {
 _putchar('\n');
 }
@@ -16,7 +16,7 @@ int i, j;
 
 for (i = 0; i < n; i++)
 {
-for (i = 0; j < n; j++)
+for (j = 0; j < n; j++)
 {
 if (j == i)
 _putchar('\\'):
