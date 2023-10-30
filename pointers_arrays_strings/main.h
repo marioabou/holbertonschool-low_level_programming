91a8b185659ea06dd7d7ddef4513d8e739a901a7
marioabou <7655@holbertonstudents.com>
2023-10-29 22:13:50 -0700

    0-strcat.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,7 +15,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-void _putchar(char c);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
