00ebc7c02fcda25455ced7f3cc8fdcefc8cf13b0
marioabou <7655@holbertonstudents.com>
2023-10-29 20:20:56 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pinter to an int as parameter
+ * updates the value in points to 98
+ * @n: imput
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
