179d3e70c9d397ba1d99a5b0c3e425399d0b02c8
marioabou <7655@holbertonstudents.com>
2023-11-05 21:17:08 -0800

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -18,7 +18,7 @@ return (actual_sqrt_recursion(n, 0));
 * @i: iterator
 * Return: the resulting square root
 */
-int actual_sqrt_recursion(int n, int i);
+int actual_sqrt_recursion(int n, int i)
 {
 if (i * i > n)
 return (-1);
