8c0f9df3395d4340e2f507b41a4d585d816de9bf
marioabou <7655@holbertonstudents.com>
2023-11-05 21:16:14 -0800

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int actual_square_recursion(int n, int i);
+int actual_sqrt_recursion(int n, int i);
 /**
 * _sqrt_recursion - returns the natural square root of a number
 * @n: number to calculate the square root of
@@ -18,12 +18,12 @@ return (actual_sqrt_recursion(n, 0));
 * @i: iterator
 * Return: the resulting square root
 */
-int actual_square_recursion(int n, int i);
+int actual_sqrt_recursion(int n, int i);
 {
 if (i * i > n)
 return (-1);
 if (i * i == n)
 return (i);
-return (actual_square_recursion(n, i + 1));
+return (actual_sqrt_recursion(n, i + 1));
 }
 
