5f91a0ef3ac18910d90070bda5844798f68b62da
marioabou <7655@holbertonstudents.com>
2023-11-05 19:07:38 -0800

    3-factorial.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _puts_recursion - function like puts();
+*@s : input
+*Return: Always 0 (sucsses)
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
+
