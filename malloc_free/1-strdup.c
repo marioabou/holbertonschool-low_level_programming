3f9f2c380f7c1936891554cf7d931c6b98caab08
marioabou <7655@holbertonstudents.com>
2023-11-12 13:20:10 -0800

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -19,7 +19,8 @@ while (str[i] != '\0')
 i++;
 aaa = malloc(sizeof(char) * (i + 1));
 if (aaa == NULL)
-return (NULL);for (r = 0; str[r]; r++)
+return (NULL);
+for (r = 0; str[r]; r++)
 aaa[r] = str[r];
 return (aaa);		
 }
