079cd61b742cb4691c091953e83b4bb79982bdf6
marioabou <7655@holbertonstudents.com>
2023-11-12 13:14:26 -0800

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,8 +8,8 @@
  * Description: creat array of size size and assign char c
  * Return: pointer to array, NULL if fail
  */
-char *create_array(unsigned int size, char c)		
-{	
+char *create_array(unsigned int size, char c)
+{
 char *str;
 unsigned int i;
 
@@ -18,6 +18,6 @@ if (size == 0 || str == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
 str[i] = c;
-return (str);		
+return (str);
 }
 
